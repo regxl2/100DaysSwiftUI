d9eb0395face142bc70ec0367862cb0546456359
regxl2 <rathourabhishekvk4@gmail.com>
2024-10-15 14:35:10 +0530

    completed upto to 60days

---
+++ SwiftDataProject/SwiftDataProject/Job.swift
@@ -0,0 +1,8 @@
+//
+//  Job.swift
+//  SwiftDataProject
+//
+//  Created by Abhishek Rathore on 15/10/24.
+//
+
+import Foundation
---
+++ SwiftDataProject/SwiftDataProject/Userview.swift
@@ -0,0 +1,18 @@
+//
+//  Userview.swift
+//  SwiftDataProject
+//
+//  Created by Abhishek Rathore on 14/10/24.
+//
+
+import SwiftUI
+
+struct Userview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Userview()
+}
